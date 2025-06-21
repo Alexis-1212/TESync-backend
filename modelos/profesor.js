2fadd9bba1757a909d29220830cb70d7c958953d
12325325 <angel_ch1@tesch.edu.mx>
2025-06-20 19:11:29 -0600

    modelo profesor

---
+++ modelos/profesor.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MaestroSchema = new Schema({
+  nombre: { type: String, required: true },
+  cedula: { type: String },
+  correo: { type: String },
+  materias: [{ type: Schema.Types.ObjectId, ref: 'Materia' }] // Materias que imparte
+});
+
+module.exports = mongoose.model('Maestro', MaestroSchema);

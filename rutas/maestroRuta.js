3b7f7278e461b6e83ad12fd1a32f569c34c73190
MrAlexisSM <mr.alexis.santiago@gmail.com>
2025-06-22 17:51:33 -0600

    actualizacion de errores de rutas de maetsro

--- rutas/maestroRuta.js
+++ rutas/maestroRuta.js
@@ -5,10 +5,8 @@ const crud = require('../controlador/maestro/maestroCrud');
 // 📦 CRUD básico de maestros
 router.post('/crear-maestro', crud.crearMaestro);
 router.get('/obtener-maestros', crud.obtenerMaestros);
-router.get('/obtener-maestro:id', crud.obtenerMaestro);
-router.put('/actualizar-maestro:id', crud.actualizarMaestro);
-router.delete('/eliminar-maestro:id', crud.eliminarMaestro);
-
-// Si deseas agregar más funciones (como materias, autenticación, etc.), aquí puedes expandir.
+router.get('/obtener-maestro/:id', crud.obtenerMaestro);
+router.put('/actualizar-maestro/:id', crud.actualizarMaestro);
+router.delete('/eliminar-maestro/:id', crud.eliminarMaestro);
 
 module.exports = router;

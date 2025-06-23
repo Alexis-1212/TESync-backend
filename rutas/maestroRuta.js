534f46119e8c0d9121d2dca41b4638d3d75686e1
12325325 <angel_ch1@tesch.edu.mx>
2025-06-22 21:04:43 -0600

    rutas maestroRuta.js

--- rutas/maestroRuta.js
+++ rutas/maestroRuta.js
@@ -5,8 +5,9 @@ const crud = require('../controlador/maestro/maestroCrud');
 // 📦 CRUD básico de maestros
 router.post('/crear-maestro', crud.crearMaestro);
 router.get('/obtener-maestros', crud.obtenerMaestros);
-router.get('/obtener-maestro/:id', crud.obtenerMaestro);
-router.put('/actualizar-maestro/:id', crud.actualizarMaestro);
-router.delete('/eliminar-maestro/:id', crud.eliminarMaestro);
+router.get('/obtener-maestro/:matricula', crud.obtenerMaestro);
+router.put('/actualizar-maestro/:matricula', crud.actualizarMaestro);
+router.delete('/eliminar-maestro/:matricula', crud.eliminarMaestro);
 
 module.exports = router;
+

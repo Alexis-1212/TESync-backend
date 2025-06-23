af47752ba7a3043531ad8c49efb926be86e4f2a9
12325325 <angel_ch1@tesch.edu.mx>
2025-06-22 20:13:31 -0600

    controlador maestro maestroCrud.js

--- controlador/maestro/maestroCrud.js
+++ controlador/maestro/maestroCrud.js
@@ -14,7 +14,7 @@ exports.crearMaestro = async (req, res) => {
 // Obtener todos los maestros
 exports.obtenerMaestros = async (req, res) => {
   try {
-    const maestros = await Maestro.find().populate('materias');
+    const maestros = await Maestro.find(); // Se quitó populate('materias')
     res.json(maestros);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -24,7 +24,7 @@ exports.obtenerMaestros = async (req, res) => {
 // Obtener un maestro por ID
 exports.obtenerMaestro = async (req, res) => {
   try {
-    const maestro = await Maestro.findById(req.params.id).populate('materias');
+    const maestro = await Maestro.findById(req.params.id); // Se quitó populate('materias')
     if (!maestro) return res.status(404).json({ error: 'Maestro no encontrado' });
     res.json(maestro);
   } catch (err) {
@@ -51,3 +51,4 @@ exports.eliminarMaestro = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+

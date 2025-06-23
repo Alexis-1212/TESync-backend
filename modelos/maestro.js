8aef800af4164093f8c5e440942894ef46a3b32b
MrAlexisSM <mr.alexis.santiago@gmail.com>
2025-06-22 20:31:18 -0600

    actualizacion de errores maestro

--- modelos/maestro.js
+++ modelos/maestro.js
@@ -5,7 +5,8 @@ const MaestroSchema = new Schema({
   nombre: { type: String, required: true },
   cedula: { type: String },
   correo: { type: String },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  materias: [{ type: Schema.Types.ObjectId, ref: 'Materia' }]
 });
 
 module.exports = mongoose.model('Maestro', MaestroSchema);
